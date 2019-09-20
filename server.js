fd8513588bfdf2b9ce33f46b7e51a74eb673127d
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-20 07:14:39 +0530

    new change

--- server.js
+++ server.js
@@ -6,20 +6,26 @@ const http = require('http');
 
 const server = http.createServer(app);
 var io = require('socket.io').listen(server);
-users =[];
-connections =[];
+users = [];
+connections = [];
 
-server.listen(process.env.PORT || 3000); 
+server.listen(process.env.PORT || 3000);
 console.log("server running");
 
-app.get('/',(req,res) =>{
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-io.sockets.on('connections',(socket)=>{
+io.sockets.on('connections', function(socket){
     connections.push(socket);
-    console.log('Connected :%s spckets connected' , connections.length);
+    console.log('Connected :%s spckets connected', connections.length);
 
-//disconnect
-connections.splice(connections.indexOf(socket),1);
-console.log('Disconnected : %s sockets connected' , connections.length);
+    socket.on('disconnect', function(data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected : %s sockets connected', connections.length);
+    });
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message',{msg:data});
+
+    });
 });
