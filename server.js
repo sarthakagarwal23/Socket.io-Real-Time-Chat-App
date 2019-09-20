f1c60cd12faa8f4f6f006d2436d97156f608f17d
Sarthak Agarwal <sarthak.agarwal97@gmail.com>
2019-09-20 06:51:12 +0530

    changes

--- server.js
+++ server.js
@@ -15,3 +15,11 @@ console.log("server running");
 app.get('/',(req,res) =>{
     res.sendFile(__dirname + '/index.html');
 });
+io.sockets.on('connections',(socket)=>{
+    connections.push(socket);
+    console.log('Connected :%s spckets connected' , connections.length);
+
+//disconnect
+connections.splice(connections.indexOf(socket),1);
+console.log('Disconnected : %s sockets connected' , connections.length);
+});
